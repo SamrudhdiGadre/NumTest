0bf75177854fcb63273e43e9cd50432b6d5683fe
Samrudhdi Gadre <Samrudhdi Gadre>
2016-11-07 12:22:02 +0530

    First Commit

---
+++ src/NumTest.java
@@ -0,0 +1,14 @@
+
+public class NumTest {
+
+	public static void main(String[] args){
+		
+		int x = 5;
+		x = x + 10;
+		System.out.println("Addition:" + x);
+		
+		int y = 5;
+		y = x * y;
+		System.out.println("Multiplication:" + y);
+	}
+}
