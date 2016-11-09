05ecf83547368a6d394b787de128b341810db67f
Samrudhdi Gadre <Samrudhdi Gadre>
2016-11-09 11:19:16 +0530

    Adding Test Runner

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -1,16 +1,18 @@
+import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 public class TestRunner {
 	
-	public static void main(String[] args) {
-	      Result result = JUnitCore.runClasses(TestJunit.class);
-			
-	      for (Failure failure : result.getFailures()) {
+	@Test
+	public void printResult(){
+		Result result = JUnitCore.runClasses(TestJunit.class);
+		  System.out.println("TestRunner Result - " );
+		for (Failure failure : result.getFailures()) {
 	         System.out.println(failure.toString());
-	      }
-			
-	      System.out.println(result.wasSuccessful());
-	   }
+		}
+	    System.out.println(result.wasSuccessful());
+	    	  
+	}
 }
