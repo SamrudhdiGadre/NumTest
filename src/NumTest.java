7c3d33fb2156f41363e077f7ab73347a25bc107e
Samrudhdi Gadre <Samrudhdi Gadre>
2016-11-24 11:03:58 +0530

    Adding Jar

--- src/NumTest.java
+++ src/NumTest.java
@@ -4,34 +4,34 @@ public class NumTest {
 public static void main(String[] args){
 				
 		int x = 5;
-		x = x + 10;
-		System.out.println("Addition:" + x);
-		
 		int y = 10;
-		y = x * y;
-		System.out.println("Multiplication:" + y);
+		int z = 15;
+		int sum = addNum(x,y);
+		System.out.println("Addition:" + sum);
 		
-		int z = 5;
-		z = y / z ;
-		System.out.println("Division:" + z);
+		System.out.println("Multiplication:" + multiplyNum(x,y));
+		
+		System.out.println("Division:" + divisionNum(z,x));
+		
+		System.out.println("Substraction:" + substractNum(z,y));
 		
 		System.out.println("Testing");
 		
 	} 
 
-public int addNum(int x, int y){
+public static int addNum(int x, int y){
 	return (x + y);
 }
 
-public int substractNum(int x, int y){
+public static int substractNum(int x, int y){
 	return (x - y);
 }
 	
-public int multiplyNum(int x, int y){
+public static int multiplyNum(int x, int y){
 	return (x * y);
 }
 
-public int divisionNum(int x, int y){
+public static int divisionNum(int x, int y){
 	return (x/y);
 }
 
