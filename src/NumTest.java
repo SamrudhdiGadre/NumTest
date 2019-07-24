f9116722677557962238997276e84af85cbaba07
SamrudhdiGadre <samrudhdi27@gmail.com>
2019-07-23 22:44:25 -0400

    Update NumTest.java
    Added some comments

--- src/NumTest.java
+++ src/NumTest.java
@@ -7,6 +7,7 @@ public static void main(String[] args){
 		int y = 10;
 		int z = 15;
 		int sum = addNum(x,y);
+		//Adding some comments
 		System.out.println("Addition:" + sum);
 		
 		System.out.println("Multiplication:" + multiplyNum(x,y));
